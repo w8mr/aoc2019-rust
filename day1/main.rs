00227255449e4523138d995148a7bb37166e3490
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2019-12-02 18:30:59 +0100

    Cleanup Day1

--- day1/main.rs
+++ day1/main.rs
@@ -5,18 +5,20 @@ use std::fs::File;
 fn main() {   
     let f = File::open("input.txt").unwrap();
     let file = BufReader::new(&f);
-//    println!("ansmer day 1: {}", file.lines().map(|s| s.unwrap().parse().unwrap()).map(|i| calc_fuel(i)).fold(0, |acc, i| acc + i));
-    println!("ansmer day 2: {}", file.lines().map(|s| s.unwrap().parse().unwrap()).map(|i| calc_fuel_recursive(i)).fold(0, |acc, i| acc + i));
+    let lines: Vec<_> = file.lines().collect();
+    let numbers: Vec<i32> = lines.iter().map(|s| s.as_ref().unwrap().parse().unwrap()).collect();
+    println!("ansmer day 1: {}", numbers.iter().map(|i| calc_fuel(i)).fold(0, |acc, i| acc + i));
+    println!("ansmer day 2: {}", numbers.iter().map(|i| calc_fuel_recursive(i)).fold(0, |acc, i| acc + i));
 }   
 
-fn calc_fuel(mass:i32) -> i32 {
+fn calc_fuel(mass:&i32) -> i32 {
     return mass / 3 - 2;
 }
 
-fn calc_fuel_recursive(mass:i32) -> i32 {
+fn calc_fuel_recursive(mass:&i32) -> i32 {
    let fuel = calc_fuel(mass);
     return match fuel {
         f if f <= 0 => 0,
-        _ => fuel + calc_fuel_recursive(fuel)
+        _ => fuel + calc_fuel_recursive(&fuel)
     }
 }
