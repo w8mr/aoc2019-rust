f2172062fc4ce7e402e0daacdc7472ce727521d7
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2019-12-04 13:48:24 +0100

    Day 4 part 1&2, boy this baby needs cleanup

---
+++ day4/src/lib.rs
@@ -0,0 +1,145 @@
+fn matches_part1(number: &i32) -> bool {
+    is_increasing(number) && has_double(number)
+}
+
+fn matches_part2(number: &i32) -> bool {
+    is_increasing(number) && has_double_which_is_not_triple(number)
+}
+
+fn is_increasing(number: &i32) -> bool {
+    let mut remainder = *number;
+    let mut result = true;
+    let mut previous: Option<i32> = None;
+    for _i in 0..6 {
+        let digit = remainder % 10;
+        remainder = remainder / 10;
+        let new = match previous {
+            Some(prev) if prev < digit => false,
+            _ => true
+        };
+        result = result && new;
+//        println!("{} {} {:#?} {} {}", digit, remainder, previous, result, new);
+        previous = Some(digit);
+    }
+    return result;
+}
+
+fn has_double(number: &i32) -> bool {
+    let mut remainder = *number;
+    let mut result = false;
+    let mut previous: Option<i32> = None;
+    for _i in 0..6 {
+        let digit = remainder % 10;
+        remainder = remainder / 10;
+        let new = match previous {
+            Some(prev) if prev == digit => true,
+            _ => false
+        };
+        result = result || new;
+//        println!("{} {} {:#?} {} {}", digit, remainder, previous, result, new);
+        previous = Some(digit);
+    }
+    return result;
+}
+
+fn has_double_which_is_not_triple(number: &i32) -> bool {
+    let mut remainder = *number;
+    let mut result = Vec::new();
+    let mut previous: Option<i32> = None;
+    let mut double_count = 1;
+    for _i in 0..6 {
+        let digit = remainder % 10;
+        remainder = remainder / 10;
+        match previous {
+            Some(prev) if prev == digit => {
+                double_count = double_count + 1;
+
+            },
+            Some(_) => {
+                result.push(double_count);
+                double_count = 1
+            }
+            None => {
+                double_count = 1
+            }
+        };
+        previous = Some(digit);
+    }
+    result.push(double_count);
+    println!("{:#?}", result);
+    return result.iter().find(|n| **n == 2).is_some();
+}
+
+
+
+pub fn part1(low: i32, high: i32) -> usize {
+//    (low..high).filter(matches_part1).for_each(|n| println!("{}",n));
+    (low..high).filter(matches_part1).count()
+}
+
+pub fn part2(low: i32, high: i32) -> usize {
+//    (low..high).filter(matches_part2).for_each(|n| println!("{}",n));
+    (low..high).filter(matches_part2).count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_matches_part1() {
+        assert_eq!(matches_part1(&111111), true);
+    }
+
+    #[test]
+    fn test_no_match_part1_not_increasing() {
+        assert_eq!(matches_part1(&223450), false);
+    }
+
+    #[test]
+    fn test_no_match_part1_no_double() {
+        assert_eq!(matches_part1(&123789), false);
+    }
+
+    #[test]
+    fn test_no_match_part1_start_decreasing() {
+        assert_eq!(matches_part1(&737999), false);
+    }
+
+    #[test]
+    fn test_match_part2() {
+        assert_eq!(matches_part2(&112233), true);
+    }
+
+    #[test]
+    fn test_no_match_part2_triple() {
+        assert_eq!(matches_part2(&123444), false);
+    }
+
+    #[test]
+    fn test_no_match_part2_triple_start() {
+        assert_eq!(matches_part2(&111234), false);
+    }
+
+    #[test]
+    fn test_match_part2_triple_double() {
+        assert_eq!(matches_part2(&111233), true);
+    }
+
+    #[test]
+    fn test_match_part2_quadruple_but_also_double() {
+        assert_eq!(matches_part2(&111122), true);
+    }
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(1331, 1345), 8);
+        //1333, 1334, 1335, 1336, 1337, 1338, 1339, 1344
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(1331, 1345), 8);
+        //1334, 1335, 1336, 1337, 1338, 1339, 1344
+    }
+
+}
---
+++ day4/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let result1 = day4::part1(246515, 739105);
+    println!("Day 4 Part 1: {}",result1);
+    let result2 = day4::part2(246515, 739105);
+    println!("Day 4 Part 2: {}",result2);
+}
