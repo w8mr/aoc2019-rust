7bd807851b8728865aa6d5c184644d11a85b2c7e
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2019-12-08 21:38:54 +0100

    Day 2

--- day2/src/lib.rs
+++ day2/src/lib.rs
@@ -1,17 +1,22 @@
 use std::collections::HashMap;
 
-
-#[derive(std::fmt::Debug)]
 struct Instruction {
-    opcode: usize,
+    opcode: Opcode,
     operand_count: usize,
     implementation: fn(Vec<usize>, &mut Context) -> Option<i32>,
 }
 
+#[derive(std::fmt::Debug, Copy, Clone)]
 enum Opcode {
     Add = 1,
     Multiply = 2,
     Halt = 99,
+}
+
+impl std::fmt::Debug for Instruction {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(fmt, "{{ opcode: {:#?}, operand_count: {} }}", self.opcode, self.operand_count)
+    }
 }
 
 fn add_implementation(operands: Vec<usize>, context: &mut Context) -> Option<i32> {
@@ -33,21 +38,31 @@ struct Context {
     memory: Vec<usize>
 }
 
-pub fn part1(opcodes: Vec<usize>) -> usize {
+pub fn part1(opcodes: &Vec<usize>) -> usize {
     let instructions = vec!(
-        Instruction { opcode: 1, operand_count: 3, implementation: add_implementation },
-        Instruction { opcode: 2, operand_count: 3, implementation: multiply_implementation },
-        Instruction { opcode: 99, operand_count: 0, implementation: halt_implementation }
+        Instruction { opcode: Opcode::Add, operand_count: 3, implementation: add_implementation },
+        Instruction { opcode: Opcode::Multiply, operand_count: 3, implementation: multiply_implementation },
+        Instruction { opcode: Opcode::Halt, operand_count: 0, implementation: halt_implementation }
     );
-    let instructions: HashMap<usize, &Instruction> = instructions.iter().map(|i| (i.opcode, i)).collect();
+    let instructions: HashMap<usize, &Instruction> = instructions.iter().map(|i| (i.opcode as usize, i)).collect();
 
-    let context = &mut Context { memory: opcodes };
-    let offset = 0;
-    let instruction = &instructions[&context.memory[offset]];
+    let context = &mut Context { memory: opcodes.to_vec() };
+    let mut offset:i32 = 0;
 
-    let x = ((offset + 1)..(offset + instruction.operand_count)).map(|i| context.memory[i]).collect();
+    while true {
+        let instruction = &instructions[&context.memory[offset as usize]];
 
-    (instruction.implementation)(x, context);
+        let x = ((offset as usize + 1)..(offset as usize + 1 + instruction.operand_count)).map(|i| context.memory[i]).collect();
+
+        match (instruction.implementation)(x, context) {
+            Some(offset_change) => {
+                offset += offset_change;
+            }
+            None => {
+                break
+            }
+        }
+    }
 
     context.memory[0]
 }
@@ -57,16 +72,23 @@ mod tests {
     use super::*;
     #[test]
     fn test_part1_opcode99() {
-        assert_eq!(part1(vec!(99)), 99);
+        assert_eq!(part1(&vec!(99)), 99);
     }
 
     #[test]
     fn test_part1_opcode1() {
-        assert_eq!(part1(vec!(1,2,3,0)), 5);
+        assert_eq!(part1(&vec!(1,5,6,0,99,2,3)), 5);
     }
 
     #[test]
     fn test_part1_opcode2() {
-        assert_eq!(part1(vec!(2,2,3,0)), 6);
+        assert_eq!(part1(&vec!(2,5,6,0,99,2,3)), 6);
     }
+
+    #[test]
+    fn test_part1_example() {
+        assert_eq!(part1(&vec!(1,9,10,3,2,3,11,0,99,30,40,50)), 3500);
+    }
+
+
 }
---
+++ day2/src/main.rs
@@ -0,0 +1,34 @@
+use std::io::BufReader;
+use std::io::BufRead;
+use std::fs::File;
+
+fn main() {
+    let f = File::open("input2.txt").unwrap();
+    let file = BufReader::new(&f);
+    let mut memory: Vec<_> = file.lines().next().unwrap().unwrap().split(",").map(|s| s.parse().unwrap()).collect();
+    memory[1]=12;
+    memory[2]=02;
+    println!("Day 2 part 1: {}",day2::part1(&memory));
+
+//1202 4138687
+//1302 4426687 288000
+//1402 4714687 576000
+
+//1203 4138688 1
+//1303 4426688 288001
+
+//0000 682685
+
+//19690720
+//720 - 685 = 35
+//19690 - 682 = 19008
+//19008 / 288 = 66
+
+// 6635
+
+
+    memory[1]=66;
+    memory[2]=35;
+    println!("Day 2 part 2: {}",day2::part1(&memory));
+}
+
