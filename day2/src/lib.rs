f9e94de1278b99cc3ccd52e0a178b65d817c554e
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2019-12-09 09:15:17 +0100

    Added unit test for input assignments. Some cleanup

--- day2/src/lib.rs
+++ day2/src/lib.rs
@@ -4,7 +4,7 @@ use std::borrow::Borrow;
 struct Instruction {
     opcode: Opcode,
     operand_count: usize,
-    implementation: fn(Vec<isize>, Vec<isize>, &mut Context) -> Option<i32>,
+    implementation: fn(Vec<isize>, Vec<isize>, &mut Context) -> Option<isize>,
 }
 
 #[derive(std::fmt::Debug, Copy, Clone)]
@@ -34,29 +34,29 @@ fn read(parameters: &Vec<isize>, modes: &Vec<isize>, context: &Context, index: u
     }
 }
 
-fn add_implementation(parameters: Vec<isize>, modes: Vec<isize>, context: &mut Context) -> Option<i32> {
+fn add_implementation(parameters: Vec<isize>, modes: Vec<isize>, context: &mut Context) -> Option<isize> {
     context.memory[parameters[2] as usize] = read(&parameters, &modes, context, 0) + read(&parameters, &modes, context, 1);
     Some(4)
 }
 
 
-fn multiply_implementation(parameters: Vec<isize>, modes: Vec<isize>, context: &mut Context) -> Option<i32>  {
+fn multiply_implementation(parameters: Vec<isize>, modes: Vec<isize>, context: &mut Context) -> Option<isize>  {
     context.memory[parameters[2] as usize] = read(&parameters, &modes, context, 0) * read(&parameters, &modes, context, 1);
     Some(4)
 }
 
-fn input_implementation(parameters: Vec<isize>, modes: Vec<isize>, context: &mut Context) -> Option<i32>  {
+fn input_implementation(parameters: Vec<isize>, modes: Vec<isize>, context: &mut Context) -> Option<isize>  {
     context.memory[parameters[0] as usize] = context.inputs.pop().unwrap();
     Some(2)
 }
 
-fn output_implementation(parameters: Vec<isize>, modes: Vec<isize>, context: &mut Context) -> Option<i32>  {
+fn output_implementation(parameters: Vec<isize>, modes: Vec<isize>, context: &mut Context) -> Option<isize>  {
     context.outputs.push(read(&parameters, &modes, context, 0));
     Some(2)
 }
 
 
-fn halt_implementation(parameters: Vec<isize>, modes: Vec<isize>, context: &mut Context) -> Option<i32>  {
+fn halt_implementation(parameters: Vec<isize>, modes: Vec<isize>, context: &mut Context) -> Option<isize>  {
     None
 }
 
@@ -86,7 +86,6 @@ pub fn day2(opcodes: &Vec<isize>) -> isize {
 }
 
 pub fn day5(opcodes: &Vec<isize>, inputs: &Vec<isize>, outputs: &mut Vec<isize>) -> isize {
-
     let instructions = vec!(
         Instruction { opcode: Opcode::Add, operand_count: 3, implementation: add_implementation },
         Instruction { opcode: Opcode::Multiply, operand_count: 3, implementation: multiply_implementation },
@@ -95,20 +94,15 @@ pub fn day5(opcodes: &Vec<isize>, inputs: &Vec<isize>, outputs: &mut Vec<isize>)
         Instruction { opcode: Opcode::Halt, operand_count: 0, implementation: halt_implementation }
     );
     let instructions: HashMap<isize, &Instruction> = instructions.iter().map(|i| (i.opcode as isize, i)).collect();
-
     let context = &mut Context { memory: opcodes.to_vec(), inputs: inputs.to_vec(), outputs: outputs };
-    let mut offset:i32 = 0;
+    let mut offset: usize = 0;
 
     loop {
-        let opcode = &context.memory[offset as usize];
-        let (opcode, modes) = split_instruction(*opcode);
-        let instruction = &instructions[&opcode];
-
-        let parameters = ((offset as usize + 1)..(offset as usize + 1 + instruction.operand_count)).map(|i| context.memory[i]).collect();
+        let (modes, instruction, parameters) = parse_instruction(&instructions, &context, offset);
 
         match (instruction.implementation)(parameters, modes, context) {
             Some(offset_change) => {
-                offset += offset_change;
+                offset = (offset as isize + offset_change) as usize;
             }
             None => {
                 break
@@ -120,9 +114,22 @@ pub fn day5(opcodes: &Vec<isize>, inputs: &Vec<isize>, outputs: &mut Vec<isize>)
     context.memory[0]
 }
 
+fn parse_instruction<'a>(instructions: &'a HashMap<isize, &Instruction>, context: &&'a mut Context, offset: usize) -> (Vec<isize>, &'a &'a Instruction, Vec<isize>) {
+    let opcode = &context.memory[offset];
+    let (opcode, modes) = split_instruction(*opcode);
+    let instruction = &instructions[&opcode];
+    let parameters = ((offset + 1)..(offset + 1 + instruction.operand_count)).map(|i| context.memory[i]).collect();
+    (modes, instruction, parameters)
+}
+
 #[cfg(test)]
 mod tests {
+    use std::io::BufReader;
+    use std::io::BufRead;
+    use std::fs::File;
+
     use super::*;
+
     #[test]
     fn test_day2_part1_opcode99() {
         assert_eq!(day2(&vec!(99)), 99);
@@ -141,6 +148,30 @@ mod tests {
     #[test]
     fn test_day2_part1_example() {
         assert_eq!(day2(&vec!(1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50)), 3500);
+    }
+
+    #[test]
+    fn test_day2_part1_assignment() {
+        let f = File::open("input2.txt").unwrap();
+        let file = BufReader::new(&f);
+        let mut memory: Vec<_> = file.lines().next().unwrap().unwrap().split(",").map(|s| s.parse().unwrap()).collect();
+
+        memory[1]=12;
+        memory[2]=02;
+        day2(&memory);
+        assert_eq!(day2(&memory), 4138687);
+    }
+
+    #[test]
+    fn test_day2_part2_assignment() {
+        let f = File::open("input2.txt").unwrap();
+        let file = BufReader::new(&f);
+        let mut memory: Vec<_> = file.lines().next().unwrap().unwrap().split(",").map(|s| s.parse().unwrap()).collect();
+
+        memory[1]=66;
+        memory[2]=35;
+        day2(&memory);
+        assert_eq!(day2(&memory), 19690720);
     }
 
     #[test]
@@ -168,5 +199,16 @@ mod tests {
     #[test]
     fn test_day5_part1_opcode_negative() {
         assert_eq!(day2(&vec!(1101,100,-1,4,0)),1101);
+    }
+
+    #[test]
+    fn test_day5_part1_assignment() {
+        let f = File::open("input5.txt").unwrap();
+        let file = BufReader::new(&f);
+        let mut memory: Vec<_> = file.lines().next().unwrap().unwrap().split(",").map(|s| s.parse().unwrap()).collect();
+
+        let mut outputs = &mut Vec::new();
+        day5(&memory, &vec!(1), outputs);
+        assert_eq!(*outputs.last().unwrap(), 16225258);
     }
 }
