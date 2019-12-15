fd637e0c654d0b1f20933c9f68047462efa884ea
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2019-12-15 23:00:30 +0100

    Refactored for part2

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 #[derive(std::fmt::Debug)]
 struct Grid {
@@ -15,7 +15,7 @@ struct Asteroid {
 }
 
 fn gcd(mut a: isize, mut b: isize) -> usize {
-    let mut t= 0;
+    let mut t;
     while b != 0 {
         t = b;
         b = a % b;
@@ -39,7 +39,6 @@ fn read_grid(input: Vec<&str>) -> Grid {
 }
 
 fn distance_angle(a: &Asteroid, b: &Asteroid) -> ((isize, isize), usize){
- //   let (a, b) = order(a, b);
     let dif_y = b.y as isize - a.y as isize;
     let dif_x = b.x as isize - a.x as isize;
     let gcd = gcd(dif_x, dif_y);
@@ -48,18 +47,6 @@ fn distance_angle(a: &Asteroid, b: &Asteroid) -> ((isize, isize), usize){
     ((dif_y, dif_x), gcd)
 }
 
-fn order<'a>(a: &'a Asteroid, b: &'a Asteroid) -> (&'a Asteroid, &'a Asteroid) {
-    if a.y < b.y { (a, b) } else if a.y > b.y { (b, a) } else {
-        if a.x < b.x { (a, b) } else { (b, a) }
-    }
-}
-
-struct VecProduct<T> {
-    a: Vec<T>,
-    b: Vec<T>,
-}
-
-
 pub fn part1(input: Vec<&str>) -> ((usize, usize), usize) {
     let grid = read_grid(input);
     let asteroids:Vec<&Asteroid> = grid.asteroids.iter().collect();
@@ -67,30 +54,40 @@ pub fn part1(input: Vec<&str>) -> ((usize, usize), usize) {
     let mut asteroids_los: Vec<((usize,usize),usize)> = asteroids.iter().map(|asteroid| ((asteroid.x, asteroid.y), count_line_of_sight(&asteroids, asteroid))).collect();
     asteroids_los.sort_by_key(|e| e.1);
     let winner = asteroids_los.last().unwrap();
-    println!("Astroid ({},{}) can see {} Astroids", (winner.0).0, (winner.0).1, winner.1);
+//    println!("Astroid ({},{}) can see {} Astroids", (winner.0).0, (winner.0).1, winner.1);
 
     *winner
 }
 
-fn count_line_of_sight(astreoids: &Vec<&Asteroid>, astroid_from: &&Asteroid) -> usize {
-    let mut angles: HashSet<(isize, isize)> = HashSet::new();
-    let mut count_duplicates = 0;
-    for astroid_to in astreoids {
-        if *astroid_from != *astroid_to {
-            let (angle, range) = distance_angle(astroid_from, astroid_to);
-            if angles.contains(&angle) {
-                count_duplicates += 1;
+fn count_line_of_sight(asteroids: &Vec<&Asteroid>, asteroid_from: &&Asteroid) -> usize {
+    partition_by_angle(asteroids, asteroid_from).len()
+}
+
+fn partition_by_angle<'a>(asteroids: &Vec<&'a Asteroid>, asteroid_from: &&Asteroid) -> HashMap<(isize, isize), Vec<(usize, &'a Asteroid)>> {
+    let mut angles: HashMap<(isize, isize), Vec<(usize, &Asteroid)>> = HashMap::new();
+    for astroid_to in asteroids {
+        if *asteroid_from != *astroid_to {
+            let (angle, range) = distance_angle(asteroid_from, astroid_to);
+            if angles.contains_key(&angle) {
+                let mut astroids_for_angle:Vec<(usize, &Asteroid)> = angles.remove(&angle).unwrap();
+                astroids_for_angle.push((range, *astroid_to));
+                astroids_for_angle.sort_by_key(|e| e.0);
+                angles.insert(angle, astroids_for_angle);
             } else {
-                angles.insert(angle);
+                angles.insert(angle, vec!((range, *astroid_to)));
             }
         }
     }
-    astreoids.len() - count_duplicates - 1
+    angles
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{read_grid, distance_angle, gcd, order, part1};
+    use std::io::{BufReader, Error};
+    use std::io::BufRead;
+    use std::fs::File;
+
+    use super::*;
 
     #[test]
     fn test_read_grid() {
@@ -135,16 +132,6 @@ mod tests {
     #[test]
     fn test_gcd_neg_2() {
         assert_eq!(gcd(-60, -24), 12);
-    }
-
-
-    #[test]
-    fn test_order() {
-        let astroids = read_grid(vec!(".#..#", ".....", "#####", "....#", "...##")).asteroids;
-        assert_eq!(order(&astroids[0], &astroids[1]).0.index, astroids[0].index);
-        assert_eq!(order(&astroids[1], &astroids[0]).0.index, astroids[0].index);
-        assert_eq!(order(&astroids[0], &astroids[2]).0.index, astroids[0].index);
-        assert_eq!(order(&astroids[2], &astroids[0]).0.index, astroids[0].index);
     }
 
     #[test]
@@ -209,5 +196,15 @@ mod tests {
         assert_eq!(c, 210);
     }
 
-
+    #[test]
+    fn test_day10_part1_assignment() {
+        let f = File::open("input10.txt").unwrap();
+        let file = BufReader::new(&f);
+        let raw_lines: Vec<Result<String, Error>> = file.lines().collect();
+        let grid_lines: Vec<&str> = raw_lines.iter().map(|l| l.as_ref().unwrap()).map(|s|s.as_str()).collect();
+        let ((x, y), c) = part1(grid_lines);
+        assert_eq!(x, 26);
+        assert_eq!(y, 29);
+        assert_eq!(c, 299);
+    }
 }
