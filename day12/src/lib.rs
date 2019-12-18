231e88ba050d7cc7069ab00fd8e80c30663a58cd
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2019-12-18 15:05:01 +0100

    Day 12 part2

--- day12/src/lib.rs
+++ day12/src/lib.rs
@@ -21,20 +21,52 @@ fn compare_position(pos1: i32, pos2: i32) -> i32 {
     else { 0 }
 }
 
-pub fn day12_part1(positions: Vec<Vec<i32>>, steps: u32) -> i32 {
+pub fn day12_part1(positions: &Vec<Vec<i32>>, steps: u32) -> i32 {
     let mut moons:Vec<Moon> = positions.iter().map(|positions| Moon::new((*positions).clone())).collect();
     for dimension in 0..3 {
-        println!("Dimension {}", dimension);
-        for step in 0..steps {
-            println!("Step {}", step + 1);
+//        println!("Dimension {}", dimension);
+        for _ in 0..steps {
+//            println!("Step {}", step + 1);
             update_velocities(&mut moons, dimension);
             update_positions(&mut moons, dimension)
         }
 
     }
     let total_energy = sum_energy(&mut moons);
-    println!("Total energy {}", total_energy);
+//    println!("Total energy {}", total_energy);
     total_energy
+}
+
+pub fn day12_part2(positions: &Vec<Vec<i32>>) -> usize {
+    let mut moons:Vec<Moon> = positions.iter().map(|positions| Moon::new((*positions).clone())).collect();
+    let steps_by_dimension: Vec<isize> = (0..3).map(|dimension| steps_to_loop_by_dimension(&mut moons, dimension)).collect();
+    lcm(lcm(steps_by_dimension[0], steps_by_dimension[1]) as isize, steps_by_dimension[2])
+}
+
+fn steps_to_loop_by_dimension(mut moons: &mut Vec<Moon>, dimension: usize) -> isize {
+    let starting_positions = moon_positions_by_dimension(&moons, dimension);
+    let mut steps = 0;
+    loop {
+
+//            println!("Step {}", step + 1);
+        update_velocities(&mut moons, dimension);
+        update_positions(&mut moons, dimension);
+        steps += 1;
+        if check_looped(&moons, &starting_positions, dimension) {
+            break;
+        }
+    }
+    //println!("Steps {} for dimension {}", steps, dimension);
+    steps
+}
+
+fn check_looped(moons: &Vec<Moon>, starting_positions: &Vec<i32>, dimension: usize) -> bool {
+    moons.iter().all(|moon| moon.velocity[dimension] == 0) &&
+        moon_positions_by_dimension(moons, dimension).iter().zip(starting_positions.iter()).all(|(&m1, &m2)| m1 == m2)
+}
+
+fn moon_positions_by_dimension(moons: &Vec<Moon>, dimension: usize) -> Vec<i32> {
+    moons.iter().map(|moon| moon.position[dimension]).collect()
 }
 
 fn sum_energy(moons: &mut Vec<Moon>) -> i32 {
@@ -57,7 +89,7 @@ fn kinetic_energy(moon: &&Moon) -> i32 {
 fn update_positions(moons: &mut Vec<Moon>, dimension:usize) -> () {
     for moon in moons {
         update_position(moon, dimension);
-        println!("Moon ({},{},{}) ({},{},{})", moon.position[0], moon.position[1], moon.position[2], moon.velocity[0], moon.velocity[1], moon.velocity[2]);
+//        println!("Moon ({},{},{}) ({},{},{})", moon.position[0], moon.position[1], moon.position[2], moon.velocity[0], moon.velocity[1], moon.velocity[2]);
     }
 }
 
@@ -88,18 +120,32 @@ pub fn parse_input_line(line: &str) -> Vec<i32> {
     caps.iter().skip(1).map(|s| s.unwrap().as_str().parse().unwrap()).collect()
 }
 
+fn gcd(mut a: isize, mut b: isize) -> usize {
+    let mut t;
+    while b != 0 {
+        t = b;
+        b = a % b;
+        a = t;
+    }
+    return a.abs() as usize
+}
+
+fn lcm(a: isize, b: isize) -> usize {
+    ((a*b)/gcd(a,b) as isize).abs() as usize
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{day12_part1, parse_input_line};
+    use super::*;
 
     #[test]
     fn day12_part1_example1() {
-        assert_eq!(day12_part1(vec!(vec!(-1,0,2), vec!(2,-10,-7), vec!(4,-8,8), vec!(3,5,-1)), 10), 179);
+        assert_eq!(day12_part1(&vec!(vec!(-1,0,2), vec!(2,-10,-7), vec!(4,-8,8), vec!(3,5,-1)), 10), 179);
     }
 
     #[test]
     fn day12_part1_example2() {
-        assert_eq!(day12_part1(vec!(vec!(-8,-10,0),vec!(5,5,10),vec!(2,-7,3),vec!(9,-8,-3)), 100), 1940);
+        assert_eq!(day12_part1(&vec!(vec!(-8,-10,0),vec!(5,5,10),vec!(2,-7,3),vec!(9,-8,-3)), 100), 1940);
     }
 
     #[test]
@@ -107,4 +153,13 @@ mod tests {
         assert_eq!(parse_input_line("<x=10, y=7, z=-9>"), vec!(10,7,-9));
     }
 
+    #[test]
+    fn day12_part2_example1() {
+        assert_eq!(day12_part2(&vec!(vec!(-1,0,2), vec!(2,-10,-7), vec!(4,-8,8), vec!(3,5,-1))), 2772);
+    }
+
+    #[test]
+    fn day12_part2_example2() {
+        assert_eq!(day12_part2(&vec!(vec!(-8,-10,0),vec!(5,5,10),vec!(2,-7,3),vec!(9,-8,-3))), 4686774924);
+    }
 }
--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,12 +1,14 @@
 use std::io::BufReader;
 use std::io::BufRead;
 use std::fs::File;
-use day12::day12_part1;
+use day12::*;
 
 fn main() {
     let f = File::open("input12.txt").unwrap();
     let file = BufReader::new(&f);
     let lines: Vec<_> = file.lines().collect();
     let numbers: Vec<Vec<i32>> = lines.iter().map(|line| day12::parse_input_line(line.as_ref().unwrap().as_str())).collect();
-    println!("Day 12 part 1: {}", day12_part1(numbers, 1000));
+    println!("Day 12 part 1: {}", day12_part1(&numbers, 1000));
+    println!("Day 12 part 2: {}", day12_part2(&numbers));
+
 }
