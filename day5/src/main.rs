650ba02f2645d21ebad55e5825db0cf8cc0f2f81
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2019-12-26 00:15:59 +0100

    Changed day5 to use method in intcode

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
     let memory = intcode::read_program_from_file("input5.txt");
 
-    let outputs = day5::run_simulation(&memory, &vec!(1));
+    let outputs = intcode::run_input_output(&memory, &vec!(1));
     let outs:Vec<String> = outputs.iter().map(|n| n.to_string()).collect();
     println!("Day 5 part 1: {}", outs.join(", "));
 
-    let outputs = day5::run_simulation(&memory, &vec!(5));
+    let outputs = intcode::run_input_output(&memory, &vec!(5));
     let outs:Vec<String> = outputs.iter().map(|n| n.to_string()).collect();
     println!("Day 5 part 2: {}", outs.join(", "));
 }
